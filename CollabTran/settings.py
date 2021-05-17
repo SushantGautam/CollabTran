94a75cc1daa70bd5e18a9a4614bad5a3288d5965
Sushant Gautam <susant.gautam@gmail.com>
2021-05-17 15:27:44 +0545

    database config

--- CollabTran/settings.py
+++ CollabTran/settings.py
@@ -72,19 +72,21 @@ WSGI_APPLICATION = 'CollabTran.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/2.1/ref/settings/#databases
 
+DATABASES = {'default': dj_database_url.config(
+    default='sqlite:////' + os.path.join(BASE_DIR, 'db.sqlite3'), conn_max_age=600)}
 
-if 'ThisIsHeroku' in os.environ:
-    # check if we are on Heroku
-    DATABASES = dj_database_url.config(conn_max_age=600)
-
-else:
-    # this is for local deployment
-    DATABASES = {
-        'default': {
-            'ENGINE': 'django.db.backends.sqlite3',
-            'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
-        }
-    }
+# if 'ThisIsHeroku' in os.environ:
+#     # check if we are on Heroku
+#     DATABASES = dj_database_url.config()
+#
+# else:
+#     # this is for local deployment
+#     DATABASES = {
+#         'default': {
+#             'ENGINE': 'django.db.backends.sqlite3',
+#             'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+#         }
+#     }
 
 # Password validation
 # https://docs.djangoproject.com/en/2.1/ref/settings/#auth-password-validators
