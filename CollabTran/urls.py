1e1de2bbb657ae14471a720f5133e532b22c77ae
Sushant Gautam <susant.gautam@gmail.com>
2021-07-10 13:18:32 +0545

    staticfiles urlpatterns

--- CollabTran/urls.py
+++ CollabTran/urls.py
@@ -13,10 +13,9 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
-from django.conf.urls.static import static
 from django.contrib import admin
-from django.contrib.staticfiles.urls import staticfiles_urlpatterns
-from django.urls import path, include
+from django.urls import path, include, re_path
+from django.views.static import serve
 
 from CollabTran import settings
 from CollabTran.views import gitpull
@@ -34,4 +33,7 @@ admin.site.site_header = 'CollabTran Administration'  # default: "Django Adminis
 admin.site.index_title = 'CollabTran Administration'  # default: "Site administration"
 admin.site.site_title = 'CollabTran Administration'  # default: "Django site admin"
 
-urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+urlpatterns += [
+    re_path(r'^static/(?P<path>.*)$', serve, {
+        'document_root': settings.STATIC_ROOT,
+    })]
